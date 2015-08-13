c482eba18ee955d78e36a363b7d26b03d46c2b91
shannonphu <shannonphu@sbcglobal.net>
2015-08-12 23:30:36 -0700

    add directions functionality

--- FindMeMilkTea/StoreDetailViewController.m
+++ FindMeMilkTea/StoreDetailViewController.m
@@ -8,9 +8,20 @@
 
 #import "StoreDetailViewController.h"
 #import "BubbleTeaStore.h"
+#import "Contact.h"
+#import "Location.h"
+#import <MapKit/MapKit.h>
 
 @interface StoreDetailViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *phoneLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *phoneIMG;
+@property (weak, nonatomic) IBOutlet UILabel *twitterLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *twitterIMG;
+@property (weak, nonatomic) IBOutlet UILabel *facebookLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *facebookIMG;
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *addressIMG;
 @end
 
 @implementation StoreDetailViewController
@@ -18,10 +29,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIImageView *backgroundImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background"]];
+    [self.view addSubview:backgroundImage];
+    [self.view sendSubviewToBack:backgroundImage];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     self.titleLabel.text = [NSString stringWithString:self.store.title];
+    if (self.store.contact.formattedPhone) {
+        self.phoneLabel.text = [NSString stringWithString:self.store.contact.formattedPhone];
+    } else {
+        self.phoneLabel.hidden = YES;
+        self.phoneIMG.hidden = YES;
+    }
+    if (self.store.contact.twitter) {
+        self.twitterLabel.text = [NSString stringWithFormat:@"@%@", self.store.contact.twitter];
+    } else {
+        self.twitterLabel.hidden = YES;
+        self.twitterIMG.hidden = YES;
+    }
+    if (self.store.contact.facebookName) {
+        self.facebookLabel.text = [NSString stringWithString:self.store.contact.facebookName];
+    } else {
+        self.facebookLabel.hidden = YES;
+        self.facebookIMG.hidden = YES;
+    }
+    self.addressLabel.text = [NSString stringWithString:self.store.subtitle];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,6 +64,18 @@
 
 - (IBAction)goBack:(id)sender {
     [self.presentingViewController dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (IBAction)getDirections:(id)sender {
+    CLLocationCoordinate2D coord = CLLocationCoordinate2DMake(self.store.location.lat.floatValue, self.store.location.lng.floatValue);
+    MKPlacemark* place = [[MKPlacemark alloc] initWithCoordinate:coord addressDictionary:nil];
+    MKMapItem* destination = [[MKMapItem alloc] initWithPlacemark: place];
+    destination.name = [NSString stringWithString:self.store.title];
+    NSArray* items = [[NSArray alloc] initWithObjects: destination, nil];
+    NSDictionary* options = [[NSDictionary alloc] initWithObjectsAndKeys:
+                             MKLaunchOptionsDirectionsModeDriving,
+                             MKLaunchOptionsDirectionsModeKey, nil];
+    [MKMapItem openMapsWithItems: items launchOptions: options];
 }
 
 /*
